3ad56856dc2ca168e54997185e2ef40b6df52656
nmortimer <31416644+nmortimer@users.noreply.github.com>
2025-08-28 08:43:29 -0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,5 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: { unoptimized: true },
+  experimental: {},
+  env: {
+    NEXT_PUBLIC_DATA_PROVIDER: process.env.DATA_PROVIDER || 'mock',
+  },
 };
+
 export default nextConfig;
